dba27e5da52c19a27cf124f6bf2b82badbb85db5
SHADESLAYER234 <praveen.weerabahu@gmail.com>
2021-03-04 07:51:56 +0530

    Added the first daily challege

---
+++ Day 1/main.js
@@ -0,0 +1,8 @@
+function takeTwoInputsDivideAndRoundDown(){
+  const firstNum = Number(prompt('Enter the first value'));
+  const secondNum = Number(prompt('Enter the second value'));
+
+   console.log(floor(firstNum / secondNum))
+}
+
+const check = takeTwoInputsDivideAndRoundDown();
