e7ecb1a6fcc2b1361bf2da0c4a623aaa12e9b54e
Shadeslayer234 <praveen.weerabahu@gmail.com>
2021-04-07 11:50:00 +0530

    Delete Day 4 directory

--- Day 4/main.js
+++
@@ -1,8 +0,0 @@
-const namesList = [];
-
-function addNewPerson(){
-  const newName = prompt('Please enter your name');
-  namesList.push(newName)
-  let position = namesList.indexOf(newName)+1
-  console.log(position)
-}
