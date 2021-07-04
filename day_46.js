eb84d2a85fc45c013abef52cc5aff07f8f0d835a
Shadeslayer234 <praveen.weerabahu@gmail.com>
2021-07-04 08:12:41 +0530

    day_46

---
+++ day_46.js
@@ -0,0 +1,21 @@
+// The First Non Repeated Character In A String
+function firstNonRepeated(s) {
+  // your code here
+  let word = s.split('')
+  let nonre = []
+  let rep = []
+  word.forEach((item,index)=>{
+    if( word.lastIndexOf(item) === index && rep.indexOf(item) === -1) {
+      nonre.push(item)
+    }
+    else{
+      rep.push(item)
+    }
+  })
+  if(nonre.length === 0){
+    return null
+  }
+  else{
+  return nonre[0]
+}
+}
